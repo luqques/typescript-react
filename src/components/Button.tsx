d4c4e85d5da70fe54c76608afa1222dccf0c1330
Lucas da Cunha <luques.cunha@gmail.com>
2025-07-07 19:17:17 -0300

    Criação de um FunctionComponent Button, recebendo uma interface de Props por parâmetro

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface ButtonProps {
+    theme?: string;
+}
+
+const Button: FunctionComponent<ButtonProps> = () => {
+    return <button></button>;
+};
+
+export default Button;
